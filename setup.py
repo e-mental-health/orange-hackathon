9d9f95dbb9c9573ce26706b3dfd732941c33952e
Tom Klaver <tomklav@gmail.com>
2019-08-29 14:09:48 +0200

    fix

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="Hackathon",
-      packages=[find_packages()],
+      packages=find_packages(),
       package_data={"orangehackathon": ["icons/*.svg"]},
       classifiers=["Example :: Invalid"],
       # Declare orangedemo package to contain widgets for the "Hackathon" category
