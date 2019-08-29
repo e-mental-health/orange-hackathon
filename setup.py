b44d13ece996479556f2b50ef84865661e6e8d1c
Tom Klaver <tomklav@gmail.com>
2019-08-29 14:08:29 +0200

    use find_packages for packages

--- setup.py
+++ setup.py
@@ -1,33 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name="Hackathon",
-      packages=["orangehackathon"],
+      packages=[find_packages()],
       package_data={"orangehackathon": ["icons/*.svg"]},
       classifiers=["Example :: Invalid"],
       # Declare orangedemo package to contain widgets for the "Hackathon" category
-      entry_points={
-          # "orange.widgets": "Hackathon = orangehackathon",
-          'orange3.addon': (
-              'hackathon = orangehackathon',
-          ),
-          # Entry point used to specify packages containing tutorials accessible
-          # from welcome screen. Tutorials are saved Orange Workflows (.ows files).
-          # 'orange.widgets.tutorials': (
-          #     # Syntax: any_text = path.to.package.containing.tutorials
-          #     'exampletutorials = orangecontrib.text.tutorials',
-          # ),
-
-          # Entry point used to specify packages containing widgets.
-          'orange.widgets': (
-              # Syntax: category name = path.to.package.containing.widgets
-              # Widget category specification can be seen in
-              #    orangecontrib/text/widgets/__init__.py
-              'Hackathon = orangehackathon.widgets',
-          ),
-
-          # Register widget help
-          # "orange.canvas.help": (
-          #     'html-index = orangecontrib.text.widgets:WIDGET_HELP_PATH',),
-
-      },
+      entry_points={"orange.widgets": "Hackathon = orangehackathon"},
       )
