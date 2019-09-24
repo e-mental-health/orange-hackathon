a3114d8905d44f544b211a04367ff9bd65dfd63b
Erik Tjong Kim Sang <erikt@xs4all.nl>
2019-09-24 18:14:17 +0200

    added x-axis selection

--- orangehackathon/widgets/lineplot.py
+++ orangehackathon/widgets/lineplot.py
@@ -16,15 +16,25 @@ class LinePlot(OWWidget):
     COLORPREFIX = "C"
     DEFAULTCOLOR = COLORPREFIX+"0"
     FIELDNAMEDATE = "date"
+    FIELDNAMENONE = "NONE"
     FIELDNAMEMSGID = "msg id"
     DEFAULTCOLOR = "C0"
     storedTable = None
     coloredColumn = -1
     plotId = 0
+    xColumn = 0
     yColumn = 0
 
     class Inputs:
         table = Input("Data", Table)
+
+    def __init__(self):
+        super().__init__()
+        self.label = gui.widgetLabel(self.controlArea)
+        self.progress = gui.ProgressBar(self, 10)
+
+    def redraw(self):
+        if self.storedTable != None: self.drawGraph()
 
     def drawWindow(self):
         form = QFormLayout()
@@ -34,28 +44,19 @@ class LinePlot(OWWidget):
         gui.widgetBox(self.controlArea, True, orientation=form)
         if self.storedTable == None: columnNames = []
         else: columnNames = sorted([x.name for x in self.storedTable.domain.variables])
-        form.addRow(
-            "y-axis:",
-            gui.radioButtons(
-                None, self, "yColumn",columnNames))
-        form.addRow(
-            "class:",
-            gui.radioButtons(
-                None, self, "coloredColumn",columnNames))
+        form.addRow("x-axis:",
+            gui.radioButtons(None, self, "xColumn",columnNames))
+        form.addRow("y-axis:",
+            gui.radioButtons(None, self, "yColumn",columnNames))
+        form.addRow("color:",
+            gui.radioButtons(None, self, "coloredColumn",columnNames+[self.FIELDNAMENONE]))
         form.addRow(gui.button(None, self, 'draw', self.redraw))
-
-    def __init__(self):
-        super().__init__()
-        self.label = gui.widgetLabel(self.controlArea)
 
     def getFieldValue(self,table,fieldName,rowId):
         for i in range(0,len(table.domain)):
             if table.domain[i].name == fieldName:
                 return(table[rowId].list[i])
         sys.exit("getFieldValue: field name not found: "+fieldName)
-
-    def redraw(self):
-        if self.storedTable != None: self.drawGraph()
 
     @Inputs.table
     def storeTable(self,table):
@@ -64,16 +65,16 @@ class LinePlot(OWWidget):
             self.drawWindow()
             self.drawGraph()
 
-    def makeColorName(self,columnName=None,columnValue=None):
-        if columnValue == None: return(self.DEFAULTCOLOR)
-        else:
-            columnValueList = list(set([self.getFieldValue(self.storedTable,columnName,i) for i in range(0,len(self.storedTable))]))
-            columnValueIndex = [i for i in range(0,len(columnValueList)) if columnValueList[i] == columnValue][0]
-            columnValueIndex = columnValueIndex % 10
-            return(self.COLORPREFIX+str(columnValueIndex))
+    def makeColorNames(self,columnName):
+        columnValueList = list(set([self.getFieldValue(self.storedTable,columnName,i) for i in range(0,len(self.storedTable))]))
+        colorNames = {}
+        for i in range(0,len(columnValueList)):
+            colorNames[columnValueList[i]] = self.COLORPREFIX+str(i % 10)
+        return(colorNames)
 
     def simplifyLegend(self,ax):
         handles, labels = ax.get_legend_handles_labels()
+        nbrOfUniqueLabels = len(set(labels))
         handlesUnique = []
         labelsUnique = []
         seen = {}
@@ -82,47 +83,44 @@ class LinePlot(OWWidget):
                 seen[labels[i]] = True
                 labelsUnique.append(labels[i])
                 handlesUnique.append(handles[i])
+                if len(labelsUnique) >= nbrOfUniqueLabels: break
         return(handlesUnique,labelsUnique)
 
     def drawGraph(self):
+        OWWidget.progressBarInit(self)
         columnNames = sorted([x.name for x in self.storedTable.domain.variables])
         self.plotId += 1
         plt.figure(self.plotId)
-        if self.coloredColumn < 0: 
-            lastMsgId = -1
-            dataX = []
-            dataY = []
-            for i in range(0,len(self.storedTable)):
-                currentMsgId = self.getFieldValue(self.storedTable,self.FIELDNAMEMSGID,i)
-                if currentMsgId != lastMsgId and len(dataX) > 0:
-                    plt.plot(dataX,dataY,color=self.makeColorName())
-                    dataX = []
-                    dataY = []
-                dataX.append(i)
-                dataY.append(self.getFieldValue(self.storedTable,columnNames[self.yColumn],i))
-                lastMsgId = currentMsgId
-            if len(dataX) > 0: 
-                plt.plot(dataX,dataY,color=self.makeColorName())
-            plt.title("y-axis: \""+columnNames[self.yColumn]+"\"")
-        else:
-            ax = plt.subplot(111)
-            lastMsgId = ""
-            lastDataValue = None
-            dataX = []
-            dataY = []
-            for i in range(0,len(self.storedTable)):
-                currentMsgId = self.getFieldValue(self.storedTable,self.FIELDNAMEMSGID,i)
-                if currentMsgId != lastMsgId and len(dataX) > 0:
-                    ax.plot(dataX,dataY,color=self.makeColorName(columnNames[self.coloredColumn],lastDataValue),label=lastDataValue)
-                    dataX = []
-                    dataY = []
-                dataX.append(i)
-                dataY.append(self.getFieldValue(self.storedTable,columnNames[self.yColumn],i))
-                lastMsgId = currentMsgId
-                lastDataValue = self.getFieldValue(self.storedTable,columnNames[self.coloredColumn],i)
-            if len(dataX) > 0:
-                plt.plot(dataX,dataY,color=self.makeColorName(columnNames[self.coloredColumn],lastDataValue),label=lastDataValue)
+        ax = plt.subplot(111)
+        lastMsgId = ""
+        lastDataValue = None
+        dataX = []
+        dataY = []
+        self.progress.iter = len(self.storedTable)
+        color = self.DEFAULTCOLOR
+        if self.coloredColumn >= 0 and self.coloredColumn < len(columnNames):
+            colorNames = self.makeColorNames(columnNames[self.coloredColumn])
+        for i in range(0,len(self.storedTable)):
+            currentMsgId = self.getFieldValue(self.storedTable,self.FIELDNAMEMSGID,i)
+            if currentMsgId != lastMsgId and len(dataX) > 0:
+                if self.coloredColumn >= 0 and self.coloredColumn < len(columnNames):
+                    color = colorNames[lastDataValue]
+                ax.plot(dataX,dataY,color=color,label=lastDataValue)
+                dataX = []
+                dataY = []
+            dataX.append(self.getFieldValue(self.storedTable,columnNames[self.xColumn],i))
+            dataY.append(self.getFieldValue(self.storedTable,columnNames[self.yColumn],i))
+            lastMsgId = currentMsgId
+            lastDataValue = self.getFieldValue(self.storedTable,columnNames[self.coloredColumn],i)
+            self.progress.advance()
+        if len(dataX) > 0:
+            if self.coloredColumn >= 0 and self.coloredColumn < len(columnNames): 
+                color = colorNames[lastDataValue]
+            plt.plot(dataX,dataY,color=color,label=lastDataValue)
+        title = "x-axis: \""+columnNames[self.xColumn]+"\""+"; y-axis: \""+columnNames[self.yColumn]
+        if self.coloredColumn >= 0 and self.coloredColumn < len(columnNames):
+            title += "\"; color: \""+columnNames[self.coloredColumn]+"\""
             handlesUnique,labelsUnique = self.simplifyLegend(ax)
             ax.legend(handlesUnique,labelsUnique)
-            plt.title("y-axis: \""+columnNames[self.yColumn]+"\"; class: \""+columnNames[self.coloredColumn]+"\"")
+        plt.title(title)
         plt.show()
