be1a0b7d99ed1bae8c01c8426aaa079a28463550
Erik Tjong Kim Sang <erikt@xs4all.nl>
2019-11-27 19:39:38 +0100

    initial version

---
+++ orangehackathon/libs/tactusloaderLIB.py
@@ -0,0 +1,117 @@
+from pathlib import Path
+
+import pandas as pd
+import xml.etree.ElementTree as ET
+import re
+
+DEFAULTDIRECTORY = "/home/erikt/projects/e-mental-health/usb/tmp/20190917"
+DEFAULTPATIENTID = "1"
+MESSAGES = "./Messages/Message"
+SENDER = "Sender"
+RECIPIENT = "Recipients"
+SENDER = "Sender"
+SUBJECT = "Subject"
+BODY = "Body"
+DATESENT = "DateSent"
+CLIENT = "CLIENT"
+COUNSELOR = "COUNSELOR"
+MAILDATEID = 3
+MAILBODYID = 5
+MAXIDLEN = 4
+INFILEPREFIX = "AdB"
+INFILESUFFIX = "-an.xml"
+
+def makeFileName(patientId):
+    if patientId == "": patientId = DEFAULTPATIENTID
+    fileName = patientId
+    while (len(fileName) < MAXIDLEN): fileName = "0"+fileName
+    return(INFILEPREFIX+fileName+INFILESUFFIX)
+
+def sentenceSplit(text):
+    tokens = text.split()
+    sentence = []
+    sentences = []
+    for token in tokens:
+        sentence.append(token)
+        if not re.search(r"[a-zA-Z0-9'\"]",token):
+            sentences.append(" ".join(sentence))
+            sentence = []
+    if len(sentence) > 0: sentences.append(" ".join(sentence))
+    return(sentences)
+
+def cleanupMails(clientMails, counselorMails):
+    clientSentenceDates = {}
+    counselorSentenceDates = {}
+    for i in range(0,len(clientMails)):
+        date = clientMails[i][MAILDATEID]
+        body = clientMails[i][MAILBODYID]
+        sentences = sentenceSplit(body)
+        for s in sentences:
+            if (s in clientSentenceDates and date < clientSentenceDates[s]) or \
+                not s in clientSentenceDates:
+                clientSentenceDates[s] = date
+    for i in range(0,len(counselorMails)):
+        date = counselorMails[i][MAILDATEID]
+        body = counselorMails[i][MAILBODYID]
+        sentences = sentenceSplit(body)
+        for s in sentences:
+            if s in clientSentenceDates and date < clientSentenceDates[s]:
+                counselorSentenceDates[s] = date
+                del(clientSentenceDates[s])
+            elif s in counselorSentenceDates and date < counselorSentenceDates[s]:
+                counselorSentenceDates[s] = date
+            elif not s in clientSentenceDates and not s in counselorSentenceDates:
+                counselorSentenceDates[s] = date
+    for i in range(0,len(clientMails)):
+        date = clientMails[i][MAILDATEID]
+        body = clientMails[i][MAILBODYID]
+    return(clientMails)
+
+def cleanupText(text):
+    if text == None: return("")
+    text = re.sub(r"\s+"," ",text)
+    text = re.sub(r"^ ","",text)
+    text = re.sub(r" $","",text)
+    return(text)
+
+def anonymizeCounselor(name):
+    if name != CLIENT: return(COUNSELOR)
+    else: return(name)
+
+def getEmailData(root,patientFileName):
+    clientMails = []
+    counselorMails = []
+    for message in root.findall(MESSAGES):
+        body = ""
+        date = ""
+        recipient = ""
+        sender = ""
+        subject = ""
+        for child in message:
+            if child.tag == SENDER: 
+                sender = anonymizeCounselor(cleanupText(child.text))
+            elif child.tag == RECIPIENT: 
+                recipient = anonymizeCounselor(cleanupText(child.text))
+            elif child.tag == DATESENT: date = cleanupText(child.text)
+            elif child.tag == SUBJECT: subject = cleanupText(child.text)
+            elif child.tag == BODY: body = cleanupText(child.text)
+        if sender == CLIENT: clientMails.append([date,sender,recipient,patientFileName,subject,body])
+        else: counselorMails.append([date,sender,recipient,patientFileName,subject,body])
+    clientMails = cleanupMails(clientMails,counselorMails)
+    counselorMails = cleanupMails(counselorMails,clientMails)
+    allMails = clientMails
+    allMails.extend(counselorMails)
+    return(sorted(allMails,key=lambda subList:subList[MAILDATEID]))
+
+def processFile(directory,patientFileName):
+    if directory == "": directory = DEFAULTDIRECTORY
+    try:
+        tree = ET.parse(directory+"/"+patientFileName)
+        root = tree.getroot()
+        return(getEmailData(root,patientFileName))
+    except:
+        print("File read error:",directory+"/"+patientFileName)
+        return([])
+
+if __name__ == "__main__":
+    pass
