34668623fa7636fb18743d1ed8ce3954254e1b34
Peter Kok <kretep@users.noreply.github.com>
2020-02-26 20:51:56 +0100

    Add conversation visualization

---
+++ orangehackathon/utils/webengineview.py
@@ -0,0 +1,38 @@
+from PyQt5 import QtCore, QtWidgets, QtWebEngineWidgets
+from io import StringIO
+
+class WebEngineView(QtWebEngineWidgets.QWebEngineView):
+    """ Extends a QWebEngineView with functionality to show an Altair chart
+    """
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.page().profile().downloadRequested.connect(self.onDownloadRequested)
+        self.windows = []
+
+    @QtCore.pyqtSlot(QtWebEngineWidgets.QWebEngineDownloadItem)
+    def onDownloadRequested(self, download):
+        if (
+            download.state()
+            == QtWebEngineWidgets.QWebEngineDownloadItem.DownloadRequested
+        ):
+            path, _ = QtWidgets.QFileDialog.getSaveFileName(
+                self, self.tr("Save as"), download.path()
+            )
+            if path:
+                download.setPath(path)
+                download.accept()
+
+    def createWindow(self, type_):
+        if type_ == QtWebEngineWidgets.QWebEnginePage.WebBrowserTab:
+            window = QtWidgets.QMainWindow(self)
+            view = QtWebEngineWidgets.QWebEngineView(window)
+            window.resize(640, 480)
+            window.setCentralWidget(view)
+            window.show()
+            return view
+
+    def updateChart(self, chart, **kwargs):
+        output = StringIO()
+        chart.save(output, "html", **kwargs)
+        self.setHtml(output.getvalue())
---
+++ orangehackathon/widgets/visconversation.py
@@ -0,0 +1,81 @@
+import sys
+import os
+from PyQt5.QtCore import QFile, QIODevice, QTextStream, QUrl, Qt, pyqtSignal
+from PyQt5.QtWidgets import QFormLayout
+from Orange.widgets.widget import OWWidget, Input
+from Orange.widgets import gui
+from Orange.data import Table
+
+import pandas as pd
+import altair as alt
+import json
+
+from ..resources import resources
+
+from ..utils.pandas_compat import table_to_frame
+from ..utils.webengineview import WebEngineView
+
+class VisConversation(OWWidget):
+    name = "Conversation visualization"
+    description = "Visualization of conversations on a timeline"
+    icon = "icons/lineplot.svg"
+    want_main_area = False
+    DAAP = "daap"
+    COLORPREFIX = "C"
+    DEFAULTCOLOR = COLORPREFIX+"0"
+    storedTable = None
+
+    class Inputs:
+        table = Input("Data", Table)
+
+    def __init__(self):
+        super().__init__()
+        self.form = QFormLayout()
+
+        # Read file from QT resources
+        stream = QFile(':vegaspec-conversation-overview.json')
+        stream.open(QIODevice.ReadOnly)
+        if not stream.exists():
+            raise Exception('Vega spec not found')
+        text = QTextStream(stream).readAll()
+        stream.close()
+
+        # Load vega-spec json file into a dictionary
+        self.dict = json.loads(text)
+
+        # Create the Altair chart
+        self.chart = alt.Chart.from_dict(self.dict)
+
+        # Show the chart in a WebEngineView
+        self.view = WebEngineView()
+        self.form.addWidget(self.view)
+        self.view.updateChart(self.chart)
+
+        gui.widgetBox(self.controlArea, True, orientation=self.form)
+
+    @Inputs.table
+    def storeTable(self,table):
+        if table != None: 
+            self.storedTable = table
+
+            # Convert Orange Table to Pandas DataFrame
+            df = table_to_frame(table)
+
+            #values = df.to_dict()
+            csv = df.to_csv()
+            print(csv)
+
+            # Store data in Vega dictionary
+            self.dict['data'].pop('url')
+            self.dict['data']['values'] = csv
+
+            # Update (recreate) the chart
+            self.chart = alt.Chart.from_dict(self.dict)
+            self.view.updateChart(self.chart)
+
+
+    def redraw(self):
+        if self.storedTable != None: self.drawGraph()
+
+    def drawGraph(self):
+        pass
